77ab6d0d76323a27c2875ced581c9c083cfc327c
Christian Grimberg <christian.grimberg@outlook.com>
2019-06-03 21:54:16 -0300

    :-1: Se quita campo del SELECT documentado.

--- Fundamentos de Bases de Datos/PlatziBlog/3.QuerysAfterInsert.sql
+++ Fundamentos de Bases de Datos/PlatziBlog/3.QuerysAfterInsert.sql
@@ -5,7 +5,6 @@ SELECT
     ,SUBSTRING(`Posts`.`Contenido`,1,40) AS `ContenidoBreve`
     ,`Posts`.`Status`
     ,`Usuarios`.`Login`
-    -- ,COUNT(*) OVER (PARTITION BY `Usuarios`.`Login`) AS `PosteosTotales`
     ,`Categorias`.`Nombre`
 FROM `PlatziBlog`.`Posts`
     INNER JOIN `PlatziBlog`.`Usuarios` ON `Usuarios`.`Id` = `Posts`.`UsuarioId`
